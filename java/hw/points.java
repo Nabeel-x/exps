1d0e674abdddae8ba017de283d003d412b927d26
Nabeel-x <kolpersky@gmail.com>
2024-01-26 21:50:42 +0530

    lol2

--- java/hw/points.java
+++ java/hw/points.java
@@ -11,6 +11,12 @@ class Point{
     double result = Math.sqrt(sqx+sqy);
     return result;
   }
+  static Point midpoint(Point p1,Point p2){
+    Point temp = new Point(0,0);
+    temp.x = (p1.x + p2.x)/2;
+    temp.y = (p1.y + p2.y)/2;
+    return temp;
+  }
   void display(){
     System.out.println("x = "+x+" y = "+y);
   }
@@ -19,9 +25,12 @@ class PointMain{
   public static void main(String args[]){
     Point p1 = new Point(3,4);
     Point p2 = new Point(6,8);
+    Point p3 = Point.midpoint(p1,p2);
     double distance = Point.distance(p2,p1);
     p1.display();
     p2.display();
     System.out.println("Distance between p1 and p2: "+distance);
+    System.out.print("Midpoint of p1 and p2: ");
+    p3.display();
   }
 }
