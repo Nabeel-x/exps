7f01d3d4edaf1bd40b2a47a2245bea7e25e33260
Nabeel-X <kolpersky@gmail.com>
2024-02-19 06:45:33 +0530

    corrected bst

--- ds/exp9.c
+++ ds/exp9.c
@@ -30,16 +30,15 @@ struct node* largest(struct node* root){
 }
 struct node* delete(struct node* root, int value) {
     if(root == NULL) return root;
-    if(value > root->data) {
-        root->right = delete(root->right, value);
-    } else if(value < root->data) {
-        root->left = delete(root->left, value);
-    } else {
+    if(value > root->data) root->right = delete(root->right, value);
+    else if(value < root->data) root->left = delete(root->left, value);
+    else {
         if(root->right == NULL) {
             struct node* temp = root->left;
             free(root);
             return temp;
-        } else if(root->left == NULL) {
+        }
+        if(root->left == NULL) {
             struct node* temp = root->right;
             free(root);
             return temp;
@@ -53,7 +52,7 @@ struct node* delete(struct node* root, int value) {
 
 
 void main(){
-    struct node *root = NULL;
+    struct node *root = NULL,*lar;
     int option, value;
     do{
         printf("1: insert\n2: search\n3: largest\n4: delete\n5: exit\nEnter option:");
@@ -72,7 +71,9 @@ void main(){
             else printf("value not found\n");
             break;
         case 3:
-            printf("Largest = %d\n",largest(root)->data);
+            lar = largest(root);
+            if(lar != NULL) printf("Largest = %d\n",lar->data);
+            else printf("Empty Tree\n");
             break;
         case 4:
             printf("Enter a value: ");
