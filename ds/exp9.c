5cae4f0f9f23b46c77c617a44514d4e8eb9128f9
Nabeel-X <kolpersky@gmail.com>
2024-02-19 05:45:27 +0530

    added ds exp 9

---
+++ ds/exp9.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *left,*right;
+};
+struct node* newNode(int value){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+struct node* insert(struct node* root,int value){
+    if(root == NULL) return newNode(value);
+    if(value > root->data) root->right = insert(root->right,value);
+    if(value < root->data) root->left = insert(root->left,value);
+    return root;
+}
+int search(struct node* root,int value){
+    if(root == NULL) return 0;
+    if(value == root->data) return 1;
+    if(value > root->data) return search(root->right,value);
+    return search(root->left,value);
+}
+struct node* largest(struct node* root){
+    if(root == NULL) return NULL;
+    if(root->right == NULL) return root;
+    return largest(root->right);
+}
+struct node* delete(struct node* root, int value) {
+    if(root == NULL) return root;
+    if(value > root->data) {
+        root->right = delete(root->right, value);
+    } else if(value < root->data) {
+        root->left = delete(root->left, value);
+    } else {
+        if(root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        } else if(root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        struct node* temp = largest(root->left);
+        root->data = temp->data;
+        root->left = delete(root->left, temp->data);
+    }
+    return root;
+}
+
+
+void main(){
+    struct node *root = NULL;
+    int option, value;
+    do{
+        printf("1: insert\n2: search\n3: largest\n4: delete\n5: exit\nEnter option:");
+        scanf("%d",&option);
+        switch(option)
+        {
+        case 1:
+            printf("Enter a value: ");
+            scanf("%d",&value);
+            root = insert(root,value);
+            break;
+        case 2:
+            printf("Enter a value: ");
+            scanf("%d",&value);
+            if(search(root,value)) printf("value found\n");
+            else printf("value not found\n");
+            break;
+        case 3:
+            printf("Largest = %d\n",largest(root)->data);
+            break;
+        case 4:
+            printf("Enter a value: ");
+            scanf("%d",&value);
+            root = delete(root,value);
+            break;
+        case 5:
+            printf("Exiting: ");
+            break;
+        default:
+            break;
+        }
+    }while(option != 5);
+}
